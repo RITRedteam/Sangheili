6214fc9c16b8d9c47b0acf6137d7298a6175bbeb
Micah J. Martin <mjm5097@rit.edu>
2019-04-18 21:43:04 -0400

    Make sure address_count is an int

--- src/config.py
+++ src/config.py
@@ -6,6 +6,9 @@ import copy
 config = copy.deepcopy(os.environ)
 config['reserve_addresses'] = os.environ.get("reserve_addresses", 'false').lower().strip() in ["true", "yes", "1", "t"]
 config['validate_addresses'] = os.environ.get("validate_addresses", 'false').lower().strip() in ["true", "yes", "1", "t"]
-
+try:
+    config['address_count'] = int(os.environ.get("address_count", 30))
+except:
+    config['address_count'] = 30
 #with open("config.yml") as fil:
 #    config = yaml.load(fil)
