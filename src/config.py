d5a9e3458bd9488cf78a467cc48fb2e35bccf0b5
Micah J. Martin <mjm5097@rit.edu>
2019-04-18 21:26:39 -0400

    Default values from environment

--- src/config.py
+++ src/config.py
@@ -4,6 +4,8 @@ import copy
 
 # Read the config from the environment
 config = copy.deepcopy(os.environ)
+config['reserve_addresses'] = os.environ.get("reserve_addresses", 'false').lower().strip() in ["true", "yes", "1", "t"]
+config['validate_addresses'] = os.environ.get("validate_addresses", 'false').lower().strip() in ["true", "yes", "1", "t"]
 
 #with open("config.yml") as fil:
 #    config = yaml.load(fil)
