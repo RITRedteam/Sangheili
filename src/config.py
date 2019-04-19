d7c5ec194b4a2bf39179ef416fefcabce757a2a6
Micah J. Martin <mjm5097@rit.edu>
2019-04-18 21:17:24 -0400

    Read the config from the environment

--- src/config.py
+++ src/config.py
@@ -1,4 +1,9 @@
-import yaml
+#import yaml
+import os
+import copy
 
-with open("config.yml") as fil:
-    config = yaml.load(fil)
+# Read the config from the environment
+config = copy.deepcopy(os.environ)
+
+#with open("config.yml") as fil:
+#    config = yaml.load(fil)
