9bf4bb7a0fd0513db1fd23f2ab03e27e775d9d3c
Micah J. Martin <mjm5097@rit.edu>
2019-04-18 22:19:40 -0400

    These lines are erroring

--- src/config.py
+++ src/config.py
@@ -4,8 +4,8 @@ import copy
 
 # Read the config from the environment
 config = copy.deepcopy(os.environ)
-config['reserve_addresses'] = os.environ.get("reserve_addresses", 'false').lower().strip() in ["true", "yes", "1", "t"]
-config['validate_addresses'] = os.environ.get("validate_addresses", 'false').lower().strip() in ["true", "yes", "1", "t"]
+#config['reserve_addresses'] = os.environ.get("reserve_addresses", 'false').lower().strip() in ["true", "yes", "1", "t"]
+#config['validate_addresses'] = os.environ.get("validate_addresses", 'false').lower().strip() in ["true", "yes", "1", "t"]
 try:
     config['address_count'] = int(os.environ.get("address_count", 30))
 except:
