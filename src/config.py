6ae77c5c004e6f53362c457cd2fc4fa5d9ca053d
micahjmartin <mjm5097@rit.edu>
2019-04-18 22:23:54 -0400

    This config is annoying

--- src/config.py
+++ src/config.py
@@ -7,7 +7,7 @@ config = copy.deepcopy(os.environ)
 #config['reserve_addresses'] = os.environ.get("reserve_addresses", 'false').lower().strip() in ["true", "yes", "1", "t"]
 #config['validate_addresses'] = os.environ.get("validate_addresses", 'false').lower().strip() in ["true", "yes", "1", "t"]
 try:
-    config['address_count'] = int(os.environ.get("address_count", 30))
+    config['address_count'] = int(os.environ.get("address_count", "30"))
 except:
     config['address_count'] = 30
 #with open("config.yml") as fil:
